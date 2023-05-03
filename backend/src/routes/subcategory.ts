69b685d54a28329fff9c60a2fabf1fbc2a437cd5
AlexC01 <acuadraq@gmail.com>
2023-05-02 22:05:43 -0600

    feat(routes): routes for Subcategories
    adding route to delete, and retrieve individual categories with the items it has

--- backend/src/routes/subcategory.ts
+++ backend/src/routes/subcategory.ts
@@ -5,10 +5,23 @@ const router = express.Router();
 
 router.get("/api/subcategories", (async (_req, res) => {
   try {
-    const subcategories = await SubCategory.find();
+    const subcategories = await SubCategory.find().populate("category");
     return res.send(subcategories);
   } catch (error) {
     return res.status(500).send("Error while fetching subcategories");
+  }
+}) as RequestHandler);
+
+router.get("/api/subcategories/:id", (async (req, res) => {
+  try {
+    const subcategory = await SubCategory.findById(req.params.id)
+      .populate("category")
+      .populate({ path: "items", populate: { path: "variations", populate: { path: "sizes", populate: "size" } } })
+      .populate({ path: "items", populate: { path: "variations", populate: { path: "color" } } });
+    if (subcategory === null) throw new Error();
+    return res.send(subcategory);
+  } catch (err) {
+    return res.status(404).send();
   }
 }) as RequestHandler);
 
@@ -19,6 +32,16 @@ router.post("/api/subcategories", (async (req, res) => {
     return res.status(201).send(subcategory);
   } catch (err) {
     return res.status(400).send(err);
+  }
+}) as RequestHandler);
+
+router.delete("/api/subcategories/:id", (async (req, res) => {
+  try {
+    const subcategory = await SubCategory.findByIdAndDelete({ _id: req.params.id });
+    if (subcategory === null) return res.status(404).send();
+    return res.send(subcategory);
+  } catch (err) {
+    return res.status(500).send();
   }
 }) as RequestHandler);
 
