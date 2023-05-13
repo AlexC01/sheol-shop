5bae67c85cc113f2d45c648e28fbb06c9f4d49f7
AlexC01 <acuadraq@gmail.com>
2023-05-13 15:03:51 -0600

    refactor(query): item Query
    Moving query to a function to retrieve all of the queries with page and limit and reuse this

---
+++ backend/src/helpers/query.ts
@@ -0,0 +1,56 @@
+import { DEFAULT_SORT_BY, DEFAULT_SORT_ORDER, SORT_VALIDATIONS } from "@constants/sorts";
+
+export const getQuery = (req: any, catId?: string) => {
+  const {
+    sortBy = DEFAULT_SORT_BY,
+    sortOrder = DEFAULT_SORT_ORDER,
+    sizes,
+    colors,
+    brands,
+    minPrice,
+    maxPrice,
+    search
+  } = req.query;
+
+  const sortByParam = SORT_VALIDATIONS.validSortBy.includes(sortBy as string) ? sortBy : DEFAULT_SORT_BY;
+  const sortOrderParam = SORT_VALIDATIONS.validSortOrder.includes(sortOrder as string) ? sortOrder : DEFAULT_SORT_ORDER;
+
+  const query = {} as any;
+
+  if (catId !== undefined) {
+    query.subcategory = catId;
+  }
+
+  if (search !== undefined) {
+    query.name = { $regex: search, $options: "i" };
+  }
+
+  if (sizes !== undefined) {
+    query.sizes = { $elemMatch: { size: { $in: [sizes] }, stock: { $gt: 0 } } };
+  } else {
+    query.totalStock = { $gt: 0 };
+  }
+
+  if (colors !== undefined) {
+    query.color = { $in: [colors] };
+  }
+
+  if (brands !== undefined) {
+    query.brand = { $in: [brands] };
+  }
+
+  if (minPrice !== undefined) {
+    query.price = { $gte: parseFloat(minPrice as string) };
+  }
+
+  if (maxPrice !== undefined) {
+    query.price = { ...query.price, $lte: parseFloat(maxPrice as string) };
+  }
+
+  return { sort: { [sortByParam]: sortOrderParam }, query };
+};
+
+export const pages = (req: any) => {
+  const { page = 1, limit = 10 } = req.query;
+  return { page, limit, skip: (page - 1) * limit };
+};
