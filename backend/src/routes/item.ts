6a51f649e7706a14ad39adbd7b88147e901d27e1
AlexC01 <acuadraq@gmail.com>
2023-05-13 14:06:46 -0600

    feat(route): iTem Route
    adding route to bring the available filters of items in the subcategory

--- backend/src/routes/item.ts
+++ backend/src/routes/item.ts
@@ -2,6 +2,7 @@ import express, { type RequestHandler } from "express";
 import multer from "multer";
 import Item from "@models/item";
 import SubCategories from "@models/subcategory";
+import mongoose from "mongoose";
 import { DEFAULT_SORT_BY, DEFAULT_SORT_ORDER, SORT_VALIDATIONS } from "@constants/sorts";
 
 const router = express.Router();
@@ -26,7 +27,8 @@ router.get("/api/items", (async (req, res) => {
         .skip((+page - 1) * +limit)
         .populate("subcategory")
         .populate({ path: "sizes", populate: { path: "size" } })
-        .populate("color");
+        .populate("color")
+        .populate("brand");
       return res.status(200).send({ totalPages, currentPage: page, items });
     }
     const items = await Item.find()
@@ -34,7 +36,8 @@ router.get("/api/items", (async (req, res) => {
       .skip((+page - 1) * +limit)
       .populate("subcategory")
       .populate({ path: "sizes", populate: { path: "size" } })
-      .populate("color");
+      .populate("color")
+      .populate("brand");
 
     return res.status(200).send({ totalPages, currentPage: page, items });
   } catch (err) {
@@ -50,6 +53,7 @@ router.get("/api/items/:id/subcategory", (async (req, res) => {
     sortOrder = DEFAULT_SORT_ORDER,
     sizes,
     colors,
+    brands,
     minPrice,
     maxPrice
   } = req.query;
@@ -76,6 +80,10 @@ router.get("/api/items/:id/subcategory", (async (req, res) => {
     query.color = { $in: [colors] };
   }
 
+  if (brands !== undefined) {
+    query.brand = { $in: [brands] };
+  }
+
   if (minPrice !== undefined) {
     query.price = { $gte: parseFloat(minPrice as string) };
   }
@@ -93,12 +101,79 @@ router.get("/api/items/:id/subcategory", (async (req, res) => {
       .limit(+limit)
       .skip((+page - 1) * +limit)
       .populate({ path: "sizes", populate: { path: "size" } })
-      .populate("color");
+      .populate("color")
+      .populate("brand");
 
     const totalPages = Math.ceil(totalDocuments / +limit);
     return res.status(200).send({ name: subcategory.name, totalPages, currentPage: page, items });
   } catch (err) {
     return res.status(500).send("Error while fetching items");
+  }
+}) as RequestHandler);
+
+router.get("/api/items/:id/subcategory/filters", (async (req, res) => {
+  const { id } = req.params;
+
+  const pipeline = [];
+
+  const subcategoryId = new mongoose.Types.ObjectId(id);
+  pipeline.push({ $match: { subcategory: subcategoryId } });
+
+  pipeline.push({
+    $lookup: {
+      from: "colors",
+      localField: "color",
+      foreignField: "_id",
+      as: "colors"
+    }
+  });
+
+  pipeline.push({
+    $lookup: {
+      from: "brands",
+      localField: "brand",
+      foreignField: "_id",
+      as: "brands"
+    }
+  });
+
+  // pipeline.push({ $unwind: "$sizes" });
+
+  pipeline.push({
+    $lookup: {
+      from: "sizes",
+      localField: "sizes.size",
+      foreignField: "_id",
+      as: "sizes.size"
+    }
+  });
+
+  pipeline.push({ $unwind: "$sizes.size" });
+
+  pipeline.push({
+    $group: {
+      _id: null,
+      colors: { $addToSet: "$colors" },
+      brands: { $addToSet: "$brands" },
+      sizes: { $addToSet: "$sizes.size" },
+      minPrice: { $min: "$price" },
+      maxPrice: { $max: "$price" }
+    }
+  });
+
+  try {
+    const results = await Item.aggregate(pipeline);
+    const availableFilters = {} as any;
+    if (results.length > 0) {
+      availableFilters.colors = results[0].colors.flat();
+      availableFilters.brands = results[0].brands.flat();
+      availableFilters.sizes = results[0].sizes;
+      availableFilters.minPrice = results[0].minPrice;
+      availableFilters.maxPrice = results[0].maxPrice;
+    }
+    return res.status(200).send(availableFilters);
+  } catch (err) {
+    return res.status(500).send("Error while fetching filters");
   }
 }) as RequestHandler);
 
@@ -121,7 +196,9 @@ router.get("/api/items/:id", (async (req, res) => {
     const item = await Item.findById(req.params.id)
       .populate("subcategory")
       .populate({ path: "sizes", populate: { path: "size" } })
-      .populate("color");
+      .populate("color")
+      .populate("brand");
+
     if (item === null) throw new Error();
     return res.send(item);
   } catch (err) {
