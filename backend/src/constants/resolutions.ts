89e1bea8451e7e01469a5b7b6003f9ba77ed65c7
AlexC01 <acuadraq@gmail.com>
2023-05-14 19:54:53 -0600

    chore(constant): constant for file resolutions

---
+++ backend/src/constants/resolutions.ts
@@ -0,0 +1,14 @@
+export const IMAGES_RESOLUTIONS = {
+  hero: {
+    width: 1280,
+    height: 720
+  },
+  avatar: {
+    width: 200,
+    height: 200
+  },
+  product: {
+    width: 600,
+    height: 600
+  }
+};
