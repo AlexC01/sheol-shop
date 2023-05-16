a8003997a53347bd2633d0bec8e96a57108642e3
AlexC01 <acuadraq@gmail.com>
2023-05-15 18:07:50 -0600

    feat(env): setting ENV for the frontend

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,20 @@
+import "./globals.css";
+import { Poppins } from "next/font/google";
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "700"]
+});
+
+export const metadata = {
+  title: "Sheol-Shop",
+  description: "Sheol-Shop your clothes store"
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body className={font.className}>{children}</body>
+    </html>
+  );
+}
