3a0b1258d159d4a9d899ff302c30bd6e49371a48
AlexC01 <acuadraq@gmail.com>
2023-05-16 17:37:29 -0600

    feat(navbar): adding drawer to the page

---
+++ frontend/app/components/drawer/Drawer.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+interface DrawerProps {
+  isOpen: boolean;
+  toggleOpen: () => void;
+  children: React.ReactNode;
+}
+
+const Drawer: React.FC<DrawerProps> = ({ isOpen, toggleOpen, children }) => {
+  return (
+    <main className="lg:hidden">
+      {isOpen && (
+        <div
+          onClick={toggleOpen}
+          className={`fixed z-40 inset-0 bg-neutral-800/70 opacity-100 transition-opacity duration-500 ease-in-out`}
+        />
+      )}
+      <div
+        onClick={e => e.stopPropagation()}
+        className={`left-0 absolute z-50 w-screen max-w-lg border-[1px] bg-white h-screen shadow-xl duration-300 ease-in-out transition-all transform ${
+          isOpen ? "-translate-x-0" : "-translate-x-full"
+        }`}
+      >
+        <div className="relative w-screen max-w-lg h-full overflow-y-scoll">{children}</div>
+      </div>
+    </main>
+  );
+};
+
+export default Drawer;
--- frontend/app/components/navbar/Navbar.tsx
+++ frontend/app/components/navbar/Navbar.tsx
@@ -1,46 +1,61 @@
 "use client";
 
+import { useState } from "react";
 import { AiOutlineMenu } from "react-icons/ai";
 import Container from "../container/Container";
 import Logo from "./Logo";
 import Options from "./Options";
 import Search from "./Search";
 import UserMenu from "./UserMenu";
+import Drawer from "../drawer/Drawer";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleOpen = () => setIsOpen(!isOpen);
+
   return (
-    <nav className="fixed w-full bg-white z-10 shadow-sm">
-      <div className="h-36 md:h-20 border-b-[1px]">
-        <Container>
-          <div className="flex items-center gap-4 xl:gap-6 justify-between">
-            <div className="block md:hidden p-3 border-[1px] border-neutral-200 rounded-full cursor-pointer hover:shadow-md transition">
-              <AiOutlineMenu size={22} />
-            </div>
-            <div className="md:hidden">
-              <Logo />
-            </div>
-            <div className="hidden md:flex items-center md:flex-1 lg:flex-none">
-              <div className="block lg:hidden p-2 border-[1px] border-neutral-200 rounded-full cursor-pointer hover:shadow-md transition">
+    <div className="relative">
+      <Drawer isOpen={isOpen} toggleOpen={toggleOpen} />
+      <nav className="fixed w-full bg-white z-10 shadow-sm">
+        <div className="h-36 md:h-20 border-b-[1px]">
+          <Container>
+            <div className="flex items-center gap-4 xl:gap-6 justify-between">
+              <div
+                onClick={toggleOpen}
+                className="block md:hidden p-3 border-[1px] border-neutral-200 rounded-full cursor-pointer hover:shadow-md transition"
+              >
                 <AiOutlineMenu size={22} />
               </div>
-              <Logo />
-            </div>
-            <div className="flex lg:flex-1 items-center md:justify-between gap-2 lg:gap-0">
-              <Options />
-              <div className="flex items-center gap-4 justify-end lg:justify-between xl:justify-end">
-                <div className="hidden md:block">
-                  <Search />
+              <div className="md:hidden">
+                <Logo />
+              </div>
+              <div className="hidden md:flex items-center md:flex-1 lg:flex-none gap-2">
+                <div
+                  onClick={toggleOpen}
+                  className="block lg:hidden p-2 border-[1px] border-neutral-200 rounded-full cursor-pointer hover:shadow-md transition"
+                >
+                  <AiOutlineMenu size={22} />
                 </div>
-                <UserMenu />
+                <Logo />
+              </div>
+              <div className="flex lg:flex-1 items-center md:justify-between gap-2 lg:gap-0">
+                <Options />
+                <div className="flex items-center gap-4 justify-end lg:justify-between xl:justify-end">
+                  <div className="hidden md:block">
+                    <Search />
+                  </div>
+                  <UserMenu />
+                </div>
               </div>
             </div>
-          </div>
-          <div className="block md:hidden mt-2">
-            <Search />
-          </div>
-        </Container>
-      </div>
-    </nav>
+            <div className="block md:hidden mt-2">
+              <Search />
+            </div>
+          </Container>
+        </div>
+      </nav>
+    </div>
   );
 };
 
