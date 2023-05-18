8e0cc7537dd8cc60e2c001edba49ec1b72bbeb75
AlexC01 <acuadraq@gmail.com>
2023-05-17 21:22:31 -0600

    feat(services): creating API Client
    Creating a service to re use it without needing to add the url over and over

---
+++ frontend/app/services/APIClient.ts
@@ -0,0 +1,35 @@
+import axios, { AxiosInstance, AxiosResponse } from "axios";
+
+class APIClient {
+  client: AxiosInstance;
+
+  constructor(url: string = `${process.env.REACT_APP_API_BASE_URL}/api`) {
+    this.client = axios.create({ baseURL: url });
+
+    this.client.interceptors.response.use(
+      response => response,
+      error => {
+        const { response } = error;
+        return this.handleError(response);
+      }
+    );
+  }
+
+  protected async get<T>(path: string): Promise<AxiosResponse<T>> {
+    return this.client.get<T, AxiosResponse<T>>(path);
+  }
+
+  protected async post<T>(path: string, data?: unknown, config?: any): Promise<AxiosResponse<T>> {
+    return this.client.post<T, AxiosResponse<T>>(path, data, config);
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  private handleError(error: any): Promise<never> {
+    if (error === null || error === undefined) {
+      return Promise.reject(error);
+    }
+    return Promise.reject(error);
+  }
+}
+
+export default APIClient;
