b2ccd577b06e59ea59bed9e4018cbefed068ecf7
AlexC01 <acuadraq@gmail.com>
2023-05-22 10:47:36 -0600

    fix(fetch): changing config fetch and dependencies

--- frontend/app/services/user.ts
+++ frontend/app/services/user.ts
@@ -10,6 +10,8 @@ export const signUp = async (body: UserSignUp) => {
 export const logIn = async (body: UserLogIn) => {
   const clientAPI = new APIClient();
   clientAPI.client.defaults.withCredentials = true;
+  clientAPI.client.defaults.headers["Access-Control-Allow-Origin"] = "*";
+  clientAPI.client.defaults.headers["Content-Type"] = "application/json";
   const response = await clientAPI.client.post("/users/login", body);
   return response.data;
 };
@@ -18,6 +20,8 @@ export const checkUser = async (cookie: string) => {
   const clientAPI = new APIClient();
   clientAPI.client.defaults.withCredentials = true;
   clientAPI.client.defaults.headers.Cookie = cookie;
+  clientAPI.client.defaults.headers["Access-Control-Allow-Origin"] = "*";
+  clientAPI.client.defaults.headers["Content-Type"] = "application/json";
   const response = await clientAPI.client.get<User>("/users");
   return response.data;
 };
@@ -25,6 +29,8 @@ export const checkUser = async (cookie: string) => {
 export const logOutUser = async () => {
   const clientAPI = new APIClient();
   clientAPI.client.defaults.withCredentials = true;
+  clientAPI.client.defaults.headers["Access-Control-Allow-Origin"] = "*";
+  clientAPI.client.defaults.headers["Content-Type"] = "application/json";
   const response = await clientAPI.client.post("/users/logout");
   return response.data;
 };
